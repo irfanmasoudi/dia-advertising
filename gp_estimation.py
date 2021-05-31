8d35e92dbf66ec766220880dd1d7fdd38f2d6eea
irfanmasoudi <irfanmasoudi@users.noreply.github.com>
2021-05-31 17:58:11 +0200

    succes

--- gp_estimation.py
+++ gp_estimation.py
@@ -19,35 +19,35 @@ y_obs = np.array([]) #observed click
 
 noise_std = 5.0
 
-for i in range (0, n_obs):
+for i in range(0, n_obs):
     new_x_obs = np.random.choice(bids, 1)
     new_y_obs = generate_observation(new_x_obs, noise_std)
-
+    
     x_obs = np.append(x_obs, new_x_obs)
     y_obs = np.append(y_obs, new_y_obs)
-
-    X = np.atleast_2d(x_obs).T #GP rogressor requires a 2d array as input, so we convert the shape
+    
+    X = np.atleast_2d(x_obs).T
     Y = y_obs.ravel()
 
     theta = 1.0
     l = 1.0
     kernel = C(theta, (1e-3, 1e3)) * RBF(l, (1e-3, 1e3))
-    gp = GaussianProcessRegressor(kernel=kernel, alpha=noise_std**2, normalize_y=True, n_restarts_optimizer=10)
+    gp = GaussianProcessRegressor(kernel=kernel, alpha = noise_std**2, normalize_y=True, n_restarts_optimizer=10) #sci-kit 0.22.1
 
     gp.fit(X,Y)
 
     x_pred = np.atleast_2d(bids).T
     y_pred, sigma = gp.predict(x_pred, return_std=True)
-
+    
     plt.figure(i)
-    plt.plot(x_pred, n(x_pred), 'r:', label = r'$n(x)$')
-    plt.plot(X.ravel(), Y, 'ro', label = u'Observed Clicks')
-    plt.plot(x_pred, y_pred, 'b-', label = u'Predicted Clicks')
+    plt.plot(x_pred, n(x_pred), 'r:', label=r'$n(x)$')
+    plt.plot(X.ravel(), Y, 'ro', label=u'Observed Clicks')
+    plt.plot(x_pred, y_pred, 'b-', label=u'Predicted Clicks')
     plt.fill(np.concatenate([x_pred, x_pred[::-1]]),
-             np.concatenate([y_pred - 1.96 * sigma, (y_pred + 1.96*sigma)[::-1]]),
-             alpha=.5, fc='b', ec='None', label = '95% conf interval')
+             np.concatenate([y_pred - 1.96*sigma, (y_pred + 1.96*sigma)[::-1]]),
+             alpha = .5, fc='b', ec='None', label = '95% conf interval')
     plt.xlabel('$x$')
     plt.ylabel('$n(x)$')
     plt.legend(loc='lower right')
-    plt.show
+    plt.show()
 
